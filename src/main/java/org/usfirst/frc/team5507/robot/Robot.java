8baf5b22963f4491ccbae73eabd8c8f920d6daff
kiki-ng <gwhsrobotics129@gmail.com>
2019-10-07 16:40:38 -0700

    test commmit

--- src/main/java/org/usfirst/frc/team5507/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team5507/robot/Robot.java
@@ -28,7 +28,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * documentation. If you change the name of this class or the package after
  * creating this project, you must also update the manifest file in the resource
  * directory.
- */
+ *///
 public class Robot extends TimedRobot {
 	public static final boolean DEBUG = true;
 	
