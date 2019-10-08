d47f8323cc9f66292db854cb2d74d66a64b7e5bb
kiki-ng <gwhsrobotics129@gmail.com>
2019-10-07 17:25:24 -0700

    arm

--- src/main/java/org/usfirst/frc/team5507/robot/commands/FrontArmDown.java
+++ src/main/java/org/usfirst/frc/team5507/robot/commands/FrontArmDown.java
@@ -9,12 +9,15 @@ package org.usfirst.frc.team5507.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class FrontArmDown extends Command {
-  public FrontArmDown() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-  }
+import org.usfirst.frc.team5507.robot.Robot;
 
+import com.kauailabs.navx.frc.AHRS;
+
+public class FrontArmDown extends Command {
+  private AHRS mNavX = new AHRS(SPI.Port.kMXP, (byte) 200);
+  public FrontArmDown() {
+    requires(Robot.m_climber);
+  };
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
@@ -23,6 +26,14 @@ public class FrontArmDown extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    if (mNavX.getAngle() < 30) //angle is not accurate, just a placeholder
+    {
+      Robot.m_climber.moveArm1(1); //speed not accurate, just a placeholder too ;D
+    }
+    else
+    {
+      Robot.m_climber.stopArm1();
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
