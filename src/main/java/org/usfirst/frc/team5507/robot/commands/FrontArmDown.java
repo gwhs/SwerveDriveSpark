a48a7f994779964f15f3577f80ffdc160d71630f
wizhu6 <winniez0526@gmail.com>
2019-10-09 15:58:59 -0700

    test

--- src/main/java/org/usfirst/frc/team5507/robot/commands/FrontArmDown.java
+++ src/main/java/org/usfirst/frc/team5507/robot/commands/FrontArmDown.java
@@ -56,7 +56,7 @@ public class FrontArmDown extends Command {
     Robot.m_climber.stop();
     isEnded = false;
   }
-
+//comment
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
